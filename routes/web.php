b1c404cb4d1d055756ae73e57ca69eed23cbe899
ahmed adel <amahmoud033@gmail.com>
2020-02-12 18:26:34 +0200

    add gamps

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
+use FarhanWazir\GoogleMaps\GMaps;
+
 Route::get('/', function () {
-    return view('welcome');
+    $config = array();
+    $config['center'] = '24.8873002,43.2711261';
+    $config['zoom'] = '6.5';
+    $config['map_height'] = "100vh";
+    $config['map_width'] = '100vw';
+    $config['scrollwheel'] = true;
+    $gmaps = new GMaps($config);
+    $gmaps->initialize($config);
+
+    //add marker
+    $marker['position'] = '24.8873002,43.2711261';
+    $marker['infowindow_content'] = 'Ayman';
+    $marker['icon'] = 'https://lh3.googleusercontent.com/proxy/FzZP28hGynqJTC--UcDBbbr3iJTJaQ_r_oDTgZ5priU5LeGK9302SBH4koMSxpMzSqcBbi6IBkd86jFj0NWcUaLA7-iqGsrq5w';
+    $gmaps->add_marker($marker);
+    $map = $gmaps->create_map();
+    return view('welcome')->with('map', $map);
 });
